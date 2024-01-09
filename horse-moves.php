9b4ebf60daf0c8dfd4a217b8c0e48e80908a29f4
Uğur Biçer <17476001+uuur86@users.noreply.github.com>
2024-01-09 16:19:57 +0000

    Fixed: Excluded impossible moves Updated: Output design

--- horse-moves.php
+++ horse-moves.php
@@ -22,21 +22,26 @@ for ( $t = 0; $t < $times; $t++ ) {
     $new_y = $y + (int) $moves[$move][0];
     $new_x = $x + (int) $moves[$move][1];
 
-    if ($new_x < 8 && $new_x >= 0 && $new_y < 8 && $new_y >= 0) {
-        $x = $new_x;
-        $y = $new_y;
-
-        $probs[$y][$x] = isset($probs[$y][$x]) ? $probs[$y][$x] + 1 : 1;
+    if ($new_x >= 8 || $new_x < 0 || $new_y >= 8 || $new_y < 0) {
+        $t--;
+        continue;
     }
-}
 
-var_dump( $probs );
+    $x = $new_x;
+    $y = $new_y;
+    $probs[$y][$x] = isset($probs[$y][$x]) ? $probs[$y][$x] + 1 : 1;
+}
 
 for ($y = 0; $y < 8; $y++) {
     echo '|';
 
     for ($x = 0; $x < 8; $x++) {
-        echo $probs[$y][$x] . '|';
+        $prob = $probs[$y][$x];
+
+        if ($prob <= 10) {
+            echo ' ';
+        }
+        echo $prob . '|';
     }
     echo "\r\n";
 }

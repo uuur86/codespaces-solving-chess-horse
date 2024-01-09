e47ee9e195ad461c66aabb8188362dc547783de2
Uğur Biçer <17476001+uuur86@users.noreply.github.com>
2024-01-09 16:03:57 +0000

    first init

--- horse-moves.php
+++ horse-moves.php
@@ -19,11 +19,24 @@ $probs = [];
 
 for ( $t = 0; $t < $times; $t++ ) {
     $move = rand(0, 7);
+    $new_y = $y + (int) $moves[$move][0];
+    $new_x = $x + (int) $moves[$move][1];
 
-    if ($new_x < 7 && $new_x >= 0 && $new_y < 7 && $new_y >= 0) {
+    if ($new_x < 8 && $new_x >= 0 && $new_y < 8 && $new_y >= 0) {
         $x = $new_x;
         $y = $new_y;
 
         $probs[$y][$x] = isset($probs[$y][$x]) ? $probs[$y][$x] + 1 : 1;
     }
+}
+
+var_dump( $probs );
+
+for ($y = 0; $y < 8; $y++) {
+    echo '|';
+
+    for ($x = 0; $x < 8; $x++) {
+        echo $probs[$y][$x] . '|';
+    }
+    echo "\r\n";
 }
